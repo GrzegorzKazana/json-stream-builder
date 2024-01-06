ea866552859da2ab085a9474f64f2384793943cc
Grzegorz Kazana <kazana.grzegorz@gmail.com>
2024-01-06 12:42:45 +0100

    test: write couple property based tests

--- src/index.property.test.ts
+++ src/index.property.test.ts
@@ -1,3 +1,163 @@
-import { describe, it, expect } from 'vitest';
+import { describe, test, expect } from 'vitest';
+import fc from 'fast-check';
 
-import { createBuilder, toJson } from './index';
+import { JsonValue, createBuilder, JsonStreamBuilder, toJson } from './index';
+
+/** Generates random json structures */
+const jsonArbitrary = fc.letrec<{ value: JsonValue }>(tie => ({
+    value: fc.oneof(
+        { arbitrary: fc.constant(null), weight: 1 },
+        { arbitrary: fc.boolean(), weight: 1 },
+        { arbitrary: fc.hexaString({ maxLength: 5 }), weight: 2 },
+        {
+            arbitrary: fc
+                .integer()
+                .filter(n => n !== -0 && Number.isFinite(n) && Number.isSafeInteger(n)),
+            weight: 2,
+        },
+        { arbitrary: fc.float().filter(n => n !== -0 && Number.isFinite(n)), weight: 2 },
+        {
+            arbitrary: fc.dictionary(fc.hexaString({ maxLength: 5 }), tie('value'), {
+                maxKeys: 10,
+            }),
+            weight: 3,
+        },
+        { arbitrary: fc.array(tie('value')), weight: 3 },
+    ),
+}));
+
+describe('json stream builder properties', () => {
+    test('value obtained from the stream should always produce the same value as input', () => {
+        return fc.assert(
+            fc.asyncProperty(jsonArbitrary.value, fc.gen(), async (value, g) => {
+                const builder = createBuilder();
+
+                walkAndRecreateUsingBuilder(value, g, builder);
+
+                const result = await toJson(builder.asStream());
+
+                expect(result).toEqual(value);
+            }),
+        );
+    });
+
+    test('value obtained from the stream should always produce the same value as input regardless of order of calls across child builders', () => {
+        return fc.assert(
+            fc.asyncProperty(
+                jsonArbitrary.value,
+                fc.gen(),
+                fc.scheduler(),
+                async (value, g, scheduler) => {
+                    const builder = createBuilder();
+
+                    await walkAndRecreateUsingBuilderAsync(value, g, scheduler, builder);
+                    await scheduler.waitAll();
+
+                    const result = await toJson(builder.asStream());
+
+                    expect(result).toEqual(value);
+                },
+            ),
+        );
+    });
+});
+
+/** Reads given json value property by property, and reconstructs it using the builder. */
+function walkAndRecreateUsingBuilder<B extends JsonStreamBuilder<any>>(
+    value: JsonValue,
+    g: fc.GeneratorValue,
+    builder: B,
+): B extends JsonStreamBuilder<infer P> ? P : never {
+    if (value === null || typeof value !== 'object') return builder.primitive(value).end();
+
+    if (Array.isArray(value)) {
+        return value
+            .reduce(
+                (acc, value) =>
+                    // Either add the value as is, without creating child builders, or construct it manually.
+                    g(fc.boolean)
+                        ? acc.addItem(value)
+                        : walkAndRecreateUsingBuilder(value, g, acc.addItem()),
+                builder.array(),
+            )
+            .end();
+    }
+
+    return Object.entries(value)
+        .reduce(
+            (acc, [key, value]) =>
+                // Either add the value as is, without creating child builders, or construct it manually.
+                g(fc.boolean)
+                    ? acc.addProperty(key, value)
+                    : walkAndRecreateUsingBuilder(value, g, acc.addProperty(key)),
+            builder.object(),
+        )
+        .end();
+}
+
+/**
+ * Reads given json value property by property, and schedules its construction using the provided scheduler.
+ * The scheduler decides in what order the calls to `.addItem`, `.addProperty` and `.primitive` across nested builders are executed.
+ * This tests for race conditions and buffering.
+ */
+async function walkAndRecreateUsingBuilderAsync<B extends JsonStreamBuilder<any>>(
+    value: JsonValue,
+    g: fc.GeneratorValue,
+    s: fc.Scheduler,
+    builder: B,
+): Promise<unknown> {
+    if (value === null || typeof value !== 'object') {
+        const scheduledTick = s.schedule(tick());
+
+        scheduledTick.then(() => builder.primitive(value).end());
+
+        return s.waitFor(scheduledTick);
+    }
+
+    if (Array.isArray(value)) {
+        const arrayBuilder = builder.array();
+        const result = value.reduce(
+            (accP, item) => {
+                return accP.then(({ allItemsAddedP }) => {
+                    const scheduledTick = s.schedule(tick());
+                    const shouldAddValueDirectly = g(fc.boolean);
+
+                    const itemAddedP = scheduledTick.then(() => {
+                        return shouldAddValueDirectly
+                            ? arrayBuilder.addItem(item)
+                            : walkAndRecreateUsingBuilderAsync(item, g, s, arrayBuilder.addItem());
+                    });
+
+                    return s.waitFor(scheduledTick).then(() => ({
+                        allItemsAddedP: [...allItemsAddedP, itemAddedP],
+                    }));
+                });
+            },
+            Promise.resolve({ allItemsAddedP: [] }) as Promise<{
+                allItemsAddedP: Promise<unknown>[];
+            }>,
+        );
+
+        return result
+            .then(({ allItemsAddedP }) => Promise.all(allItemsAddedP.map(p => s.waitFor(p))))
+            .then(() => arrayBuilder.end());
+    }
+
+    const objectBuilder = builder.object();
+
+    const allPropertiesAddedP = Object.entries(value).map(([key, value]) => {
+        const shouldAddValueDirectly = g(fc.boolean);
+
+        return s.schedule(tick()).then(() => {
+            return shouldAddValueDirectly
+                ? objectBuilder.addProperty(key, value)
+                : walkAndRecreateUsingBuilderAsync(value, g, s, objectBuilder.addProperty(key));
+        });
+    });
+
+    return Promise.all(allPropertiesAddedP.map(p => s.waitFor(p))).then(() => objectBuilder.end());
+}
+
+function tick() {
+    return Promise.resolve();
+}
--- src/index.ts
+++ src/index.ts
@@ -2,18 +2,31 @@ import { PassThrough, Readable } from 'stream';
 
 export type JsonPrimitive = null | string | number | boolean;
 export type JsonValue = JsonPrimitive | Array<JsonValue> | { [k: string]: JsonValue };
+export type JsonBuilder =
+    | JsonStreamBuilder<any>
+    | ArrayStreamBuilder<any>
+    | ObjectStreamBuilder<any>
+    | ValueStreamBuilder<any>;
 
+/**
+ * Creates instance of a builder. Can be used to start emiting json chunks of primitives/arrays/objects.
+ */
 export function createBuilder() {
     return new JsonStreamBuilder(null);
 }
 
-abstract class Builder<ParentBuilder extends Builder<any> | null> {
+/** Base class, manages the stream queueing and scheduling */
+abstract class Builder<Parent extends Builder<any> | null> {
+    /** Output stream, to which we write json contents in accordance to queue mechanism */
     protected readonly stream = new PassThrough();
+    /** List of Builders that need to be processed in order. */
     protected readonly queue: Array<Builder<any>> = [];
+    /** Currently processed stream (represented via Builder) */
     protected current: Builder<any> | null = null;
+    /** Once the queue is drained, and the value set to true, the stream exposed to the user will emit `end` event */
     protected endScheduled = false;
 
-    constructor(protected readonly parent: ParentBuilder) {}
+    constructor(protected readonly parent: Parent) {}
 
     public asStream(): Readable {
         return this.stream;
@@ -25,6 +38,7 @@ abstract class Builder<ParentBuilder extends Builder<any> | null> {
         return this.consumeChildBuilder();
     }
 
+    /** Sequentially subscribes scheduled streams. Think of `concat` function in rxjs. */
     private consumeChildBuilder(): this {
         if (this.current) return this;
 
@@ -56,18 +70,21 @@ abstract class Builder<ParentBuilder extends Builder<any> | null> {
     }
 }
 
-export class JsonStreamBuilder<
-    ParentBuilder extends Builder<any> | null,
-> extends Builder<ParentBuilder> {
-    public primitive(data: JsonPrimitive): JsonStreamBuilder<ParentBuilder> {
+/** Default builder, wrapper around all builders for all types */
+export class JsonStreamBuilder<Parent extends Builder<any> | null> extends Builder<Parent> {
+    /** Pushes primitive value to stream immediately */
+    public primitive(data: JsonPrimitive): this {
         return this.value(data);
     }
 
-    public object(data: Record<string, JsonValue>): JsonStreamBuilder<ParentBuilder>;
-    public object(): ObjectStreamBuilder<ParentBuilder>;
-    public object(
-        data?: Record<string, JsonValue>,
-    ): JsonStreamBuilder<ParentBuilder> | ObjectStreamBuilder<ParentBuilder> {
+    /** Pushes data to stream immediately */
+    public object(data: Record<string, JsonValue>): this;
+    /**
+     * Creates a child builder for objects, which properties can be added any time.
+     * Until the child object is finished (via `.end()` call), all other calls are queued.
+     */
+    public object(): ObjectStreamBuilder<Parent>;
+    public object(data?: Record<string, JsonValue>): this | ObjectStreamBuilder<Parent> {
         if (data) return this.value(data);
 
         const builder = new ObjectStreamBuilder(this.parent);
@@ -76,46 +93,58 @@ export class JsonStreamBuilder<
         return builder;
     }
 
-    public array(data: Array<JsonValue>): JsonStreamBuilder<ParentBuilder>;
-    public array(): ArrayStreamBuilder<ParentBuilder>;
-    public array(
-        data?: Array<JsonValue>,
-    ): JsonStreamBuilder<ParentBuilder> | ArrayStreamBuilder<ParentBuilder> {
+    /** Pushes data to stream immediately */
+    public array(data: Array<JsonValue>): this;
+    /**
+     * Creates a child builder for array, which items can be added any time.
+     * Until the child array is finished (via `.end()` call), all other calls are queued.
+     */
+    public array(): ArrayStreamBuilder<Parent>;
+    public array(data?: Array<JsonValue>): this | ArrayStreamBuilder<Parent> {
         if (data) return this.value(data);
 
-        const builder = new ArrayStreamBuilder<ParentBuilder>(this.parent);
+        const builder = new ArrayStreamBuilder<Parent>(this.parent);
         this.addChildBuilder(builder).scheduleEnd();
 
         return builder;
     }
 
-    private value(data: JsonValue): JsonStreamBuilder<ParentBuilder> {
-        const builder = new ValueStreamBuilder<ParentBuilder>(this.parent).value(data);
+    public end(): Parent {
+        return this.parent;
+    }
+
+    private value(data: JsonValue): this {
+        const builder = new ValueStreamBuilder<Parent>(this.parent).value(data);
         this.addChildBuilder(builder).scheduleEnd();
 
         return this;
     }
 }
 
-export class ObjectStreamBuilder<
-    ParentBuilder extends Builder<any> | null,
-> extends Builder<ParentBuilder> {
+/**
+ * Builder for all json objects.
+ * Properties of objects can be added asynchronously.
+ * Building the object should be finalized via `.end()` call.
+ */
+export class ObjectStreamBuilder<Parent extends Builder<any> | null> extends Builder<Parent> {
     private firstPropertyInserted: boolean = false;
 
-    constructor(parent: ParentBuilder) {
+    constructor(parent: Parent) {
         super(parent);
         this.addChildBuilder(new ValueStreamBuilder(this).rawValue('{'));
     }
 
-    public addProperty(key: string): JsonStreamBuilder<ObjectStreamBuilder<ParentBuilder>>;
-    public addProperty(key: string, value: JsonValue): ObjectStreamBuilder<ParentBuilder>;
-    public addProperty(
-        key: string,
-        value?: JsonValue,
-    ): JsonStreamBuilder<ObjectStreamBuilder<ParentBuilder>> | ObjectStreamBuilder<ParentBuilder> {
+    /**
+     * Creates a child builder which result will be stored under specified `key`.
+     * Subsequent calls to `addProperty` are queued, and data is writted in order.
+     */
+    public addProperty(key: string): JsonStreamBuilder<this>;
+    /** Adds value under specified `key` immediately */
+    public addProperty(key: string, value: JsonValue): this;
+    public addProperty(key: string, value?: JsonValue): JsonStreamBuilder<this> | this {
         if (value !== undefined) return this.pushProperty(key, value);
 
-        const builder = new JsonStreamBuilder<ObjectStreamBuilder<ParentBuilder>>(this);
+        const builder = new JsonStreamBuilder<this>(this);
 
         this.insertCommaIfNeeded();
         this.addChildBuilder(new ValueStreamBuilder(this).rawValue(`"${key}":`));
@@ -123,14 +152,16 @@ export class ObjectStreamBuilder<
 
         return builder;
     }
-    public end(): ParentBuilder {
+
+    /** Finalizes the creation of json object. Waits for all subproperties to be resolved. */
+    public end(): Parent {
         this.addChildBuilder(new ValueStreamBuilder(this).rawValue('}'));
         this.scheduleEnd();
 
         return this.parent;
     }
 
-    private pushProperty(key: string, value: JsonValue): ObjectStreamBuilder<ParentBuilder> {
+    private pushProperty(key: string, value: JsonValue): this {
         this.insertCommaIfNeeded();
         this.addChildBuilder(
             new ValueStreamBuilder(this).rawValue(`"${key}":${JSON.stringify(value)}`),
@@ -149,38 +180,41 @@ export class ObjectStreamBuilder<
     }
 }
 
-export class ArrayStreamBuilder<
-    ParentBuilder extends Builder<any> | null,
-> extends Builder<ParentBuilder> {
+export class ArrayStreamBuilder<Parent extends Builder<any> | null> extends Builder<Parent> {
     private firstItemInserted: boolean = false;
 
-    constructor(parent: ParentBuilder) {
+    constructor(parent: Parent) {
         super(parent);
         this.addChildBuilder(new ValueStreamBuilder(this).rawValue('['));
     }
 
-    public addItem(): JsonStreamBuilder<ArrayStreamBuilder<ParentBuilder>>;
-    public addItem(value: JsonValue): ArrayStreamBuilder<ParentBuilder>;
-    public addItem(
-        value?: JsonValue,
-    ): JsonStreamBuilder<ArrayStreamBuilder<ParentBuilder>> | ArrayStreamBuilder<ParentBuilder> {
+    /**
+     * Creates a child builder which is responsible for creating the array item.
+     * Subsequent calls to `addItem` are queued, and data is writted in order.
+     */
+    public addItem(): JsonStreamBuilder<this>;
+    /** Adds array item immediately */
+    public addItem(value: JsonValue): this;
+    public addItem(value?: JsonValue): JsonStreamBuilder<this> | this {
         if (value !== undefined) return this.pushItem(value);
 
-        const builder = new JsonStreamBuilder<ArrayStreamBuilder<ParentBuilder>>(this);
+        const builder = new JsonStreamBuilder<this>(this);
 
         this.insertCommaIfNeeded();
         this.addChildBuilder(builder);
 
         return builder;
     }
-    public end(): ParentBuilder {
+
+    /** Finalizes the creation of the array. Waits for all items created via child builders to be resolved. */
+    public end(): Parent {
         this.addChildBuilder(new ValueStreamBuilder(this).rawValue(']'));
         this.scheduleEnd();
 
         return this.parent;
     }
 
-    private pushItem(value: JsonValue): ArrayStreamBuilder<ParentBuilder> {
+    private pushItem(value: JsonValue): this {
         this.insertCommaIfNeeded();
         this.addChildBuilder(new ValueStreamBuilder(this).value(value));
 
@@ -197,14 +231,13 @@ export class ArrayStreamBuilder<
     }
 }
 
-export class ValueStreamBuilder<
-    ParentBuilder extends Builder<any> | null,
-> extends Builder<ParentBuilder> {
-    public value(data: JsonValue): ValueStreamBuilder<ParentBuilder> {
+/** Dummy builder for writing primitives. Does not create any child builders. */
+export class ValueStreamBuilder<Parent extends Builder<any> | null> extends Builder<Parent> {
+    public value(data: JsonValue): this {
         return this.rawValue(JSON.stringify(data));
     }
 
-    public rawValue(data: string): ValueStreamBuilder<ParentBuilder> {
+    public rawValue(data: string): this {
         this.stream.push(data);
         this.stream.push(null);
 
@@ -212,6 +245,7 @@ export class ValueStreamBuilder<
     }
 }
 
+/** Utility (mostly for testing) collecting values of the stream and parsing them. */
 export function toJson(stream: Readable): Promise<JsonValue> {
     return new Promise((resolve, reject) => {
         const chunks: string[] = [];
